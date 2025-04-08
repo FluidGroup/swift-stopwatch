aa727bfd9fe32453b7c737023d7d136eaf0deed5
Muukii <muukii.app@gmail.com>
2025-04-08 23:01:40 +0900

    Update

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-  name: "swift-stopwatch",
+  name: "Stopwatch",
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
-      name: "swift-stopwatch",
-      targets: ["swift-stopwatch"])
+      name: "Stopwatch",
+      targets: ["Stopwatch"])
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "swift-stopwatch")
+      name: "Stopwatch")
 
   ]
 )
